d11010310089961e531f891d60823ee204def41d
Collin Giess <giesscollin@gmail.com>
2024-01-13 00:44:25 -0500

    Added Swagger configuration to API

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,11 @@
-import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import PostRepository from './repositories/posts/PostRepository';
+
+const swaggerOptions = {};
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 if (process.env.NODE_ENV == 'local') {
     dotenv.config({ path: '.env.local' });
@@ -15,11 +20,22 @@ const port: number = isNaN(Number(process.env.PORT))
     ? 5000
     : Number(process.env.PORT);
 
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Hello World
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 app.get('/', async (request: Request, response: Response) => {
     await new PostRepository().getAll();
+    response.send('Hello World!');
 });
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-
