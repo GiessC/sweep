aba5896c6bbbe56a63d9c288b7484d41971b8768
GiessC <68926012+GiessC@users.noreply.github.com>
2024-01-13 22:24:58 -0500

    Swagger Configuration for API (#8)

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,30 @@
-import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import PostRepository from './repositories/posts/PostRepository';
+import express, { Express } from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { version } from '../package.json';
+import posts from './routes/Post';
+
+const swaggerOptions: swaggerJSDoc.Options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Sweep API',
+            version,
+            contact: {
+                email: 'giesscollin@gmail.com',
+                name: 'Collin Giess',
+            },
+            description: 'API for the sweep social media apps',
+            license: {
+                name: 'MIT',
+                url: 'https://opensource.org/licenses/MIT',
+            },
+        },
+    },
+    apis: ['./src/routes/*.ts'],
+};
+const openapiSpecification = swaggerJSDoc(swaggerOptions);
 
 if (process.env.NODE_ENV == 'local') {
     dotenv.config({ path: '.env.local' });
@@ -15,11 +39,9 @@ const port: number = isNaN(Number(process.env.PORT))
     ? 5000
     : Number(process.env.PORT);
 
-app.get('/', async (request: Request, response: Response) => {
-    await new PostRepository().getAll();
-});
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+app.use('/post', posts);
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-
