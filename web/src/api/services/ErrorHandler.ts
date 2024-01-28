5a24fc62596cc16bb9c60409cb481e3b525c6a77
Collin Giess <giesscollin@gmail.com>
2024-01-28 14:22:45 -0500

    Nice

--- web/src/api/services/ErrorHandler.ts
+++ web/src/api/services/ErrorHandler.ts
@@ -1,5 +1,5 @@
 import APIResponse from '../APIResponse';
-import { UNKNOWN } from '../exceptions/errorMessages';
+import { UNKNOWN } from '../exceptions/ErrorMessages';
 
 export default class ErrorHandler {
     public static handleError<T = unknown>(error: unknown): APIResponse<T> {
